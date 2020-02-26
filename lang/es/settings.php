75b1baaa37bda0dafd040f3d20c8c141dc923140
Joaquín Herrero Pintado <joakinen@gmail.com>
2020-02-26 12:37:40 +0100

    Create settings.php

---
+++ lang/es/settings.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['maxshowen'] = 'Máximo número de páginas mostradas en Quality Summary';
+$lang['adminonly'] = 'Quality Summery solo visible para miembros del grupo admin';
+$lang['single_author_only'] = 'Solo hay un autor o muy pocos.';
